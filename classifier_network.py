8b6c87a1e18393b5843b506d76b3254cd2258899
kidusmik <kidusmik@gmail.com>
2022-10-27 23:35:30 +0300

    Add documentation for the classifier network

--- classifier_network.py
+++ classifier_network.py
@@ -1,19 +1,24 @@
+""" Definition of Network class """
 import torch
 from torch import nn
 import torch.nn.functional as F
 
 
+# Most of this is taken from the class notes
 class Network(nn.Module):
     def __init__(self, input_size, output_size, hidden_layers, drop_p=0.2):
-        ''' Builds a feedforward network with arbitrary hidden layers.
+        """
+        Builds a feedforward network with arbitrary hidden layers.
         
-            Arguments
-            ---------
-            input_size: integer, size of the input layer
-            output_size: integer, size of the output layer
-            hidden_layers: list of integers, the sizes of the hidden layers
+        Attributes:
+            - hidden_layers (list): The modules of the hidden layers
+            
+        Arguments:
+            - input_size (int): Size of the input layer
+            - output_size (int): Size of the output layer
+            - hidden_layers (list): The sizes of the hidden layers
         
-        '''
+        """
         super().__init__()
         # Input to a hidden layer
         self.hidden_layers = nn.ModuleList([nn.Linear(input_size, hidden_layers[0])])
@@ -27,7 +32,14 @@ class Network(nn.Module):
         self.dropout = nn.Dropout(p=drop_p)
         
     def forward(self, x):
-        ''' Forward pass through the network, returns the output logits '''
+        """
+        Forward pass through the network, returns the output logits
+        
+        Arguments:
+            x (tensor): The tensor to forward pass through the model
+        
+        Returns: The log Softmax of the output
+        """
         
         for each in self.hidden_layers:
             x = F.relu(each(x))
